b19e0d5d163f908adef09650113696aa598d7d01
Lucija Fekonja <lucija.fekonja@gmail.com>
2021-05-09 00:16:25 +0200

    Verjetno končna verzija

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -2,9 +2,13 @@ import bottle
 import model
 import datetime
 import json
+import os
 
+SKRIVNOST = 'banana'
 seznam_uporabnikov = model.VsiUporabniki('uporabniki')
+slovar_hrane = model.Seznam()
 
+#######################################  LOGIN PAGE   #######################################
 @bottle.get('/')
 def index():
     return bottle.template('login.tpl')
@@ -13,14 +17,41 @@ def index():
 def login():
     ime = bottle.request.forms.getunicode('ime')
     geslo = bottle.request.forms.getunicode('geslo')
-    uporabnik = seznam_uporabnikov.uporabniki[ime]
-    id_dneva = uporabnik.id_dneva
+    datum = str(datetime.date.today())
+    datum_list = [int(x) for x in datum.split('-')]
 
-    if geslo == uporabnik.geslo:
-        return bottle.redirect('/front-page/{}/{}'.format(ime, id_dneva))
-    else:
+    if seznam_uporabnikov.uporabnik_obstaja(list(seznam_uporabnikov.uporabniki.keys()), ime, geslo) == False:
+        # Preverimo, če uporabnik sploh obstaja. Če ne obstaja... 
         return bottle.redirect('/')
+    else:
+        uporabnik = seznam_uporabnikov.uporabniki[ime]
+        dnevi = uporabnik.seznam_dni
+        if seznam_uporabnikov.tapravi_datum(dnevi, [int(x) for x in datum.split('-')]) == False:
+            # Če obstaja, preverimo, če ima vnesene kakšne vrednosti za danes. 
+            # Če nima, naredimo nov dan. 
+            dan = uporabnik.seznam_dni[-1]
+            vrednosti_zadnjega = {'teza' : dan.teza,
+                                  'visina' : dan.visina,
+                                  'starost' : dan.starost,
+                                  'spol' : dan.spol,
+                                  'aktivnost' : dan.aktivnost                             
+                                  }
+            uporabnik.nov_dan(dan.teza,
+                              dan.visina,
+                              dan.starost,
+                              dan.spol,
+                              dan.aktivnost,
+                              datum_list
+                              )
+            seznam_uporabnikov.shrani(ime)
+            bottle.response.set_cookie('ime', '{}'.format(ime), secret=SKRIVNOST, path='/')
+            return bottle.redirect('/front-page/{}/{}'.format(ime, datum))
+        else:
+            # Če ima, vrnemo stran z že vnešenimi podatki. 
+            return bottle.redirect('/front-page/{}/{}'.format(ime, datum))
 
+
+#######################################  SIGN IN PAGE   #######################################
 
 @bottle.get('/vpis/')
 def sign_in():
@@ -35,24 +66,160 @@ def sign_in():
     starost = float(bottle.request.forms.getunicode('starost'))
     spol = str(bottle.request.forms.getunicode('spol'))
     aktivnost = str(bottle.request.forms.getunicode('aktivnost'))
-    datum = datetime.date.today()
- 
-    seznam_uporabnikov.nov_uporabnik(ime, geslo)
-    seznam_uporabnikov.shrani(ime)    
+    datum_str = datetime.date.today()
+    datum = [int(x) for x in str(datum_str).split('-')]
 
-    uporabnik = seznam_uporabnikov.uporabniki[ime]
-    uporabnik.nov_dan(teza, visina, starost, spol, aktivnost, datum)
-    id = uporabnik.id_dneva
+    seznam_uporabnikov.nov_uporabnik(ime, geslo)       # ustvarimo novega uporabnika iz podatkov
+    uporabnik = seznam_uporabnikov.uporabniki[ime]     # dostopamo do uporabnika
+    uporabnik.nov_dan(teza, visina, starost, spol, aktivnost, datum)   # dodamo nov dan na seznam_dni
+    seznam_uporabnikov.shrani(ime)    #shranimo v json v mapi 'uporabniki'
 
-    if seznam_uporabnikov.preveri_ime_geslo(ime, geslo) is True:
-        return bottle.redirect('/front-page/{}/{}'.format(ime, id))
+    return bottle.redirect('/front-page/{}/{}'.format(ime, datum_str))
+
+
+#######################################  FRONT PAGE   #######################################
+
+@bottle.get('/front-page/<ime_uporabnika>/<datum>')
+def front_page(ime_uporabnika, datum):
+    datum_list = [int(x) for x in datum.split('-')]
+    seznam_vrednosti = dnevni_seznam_vrednosti(datum_list, ime_uporabnika)
+    slovar_hrane = dnevni_slovar_hrane(datum_list, ime_uporabnika)
+    datum_str = '. '.join([str(x) for x in datum.split('-')[::-1]])
+
+    return bottle.template('views/front-page.tpl', ime_uporabnika=ime_uporabnika,
+                                                   datum=datum,
+                                                   seznam_vrednosti=seznam_vrednosti,
+                                                   slovar_hrane=slovar_hrane,
+                                                   datum_str=datum_str)
+
+@bottle.post('/fp-izberi-dan/')
+def front_page_dneva(): 
+    ime = str(bottle.request.get_cookie('ime', secret=SKRIVNOST))
+    uporabnik = seznam_uporabnikov.uporabniki.get(ime)
+    dnevi = uporabnik.seznam_dni
+    datum_str  = bottle.request.forms.getunicode('datum')
+    datum = [int(x) for x in datum_str.split('-')]
+
+    if seznam_uporabnikov.tapravi_datum(dnevi, datum) == False:
+        dan = uporabnik.seznam_dni[-1]
+        vrednosti_zadnjega = {'teza' : dan.teza,
+                              'visina' : dan.visina,
+                              'starost' : dan.starost,
+                              'spol' : dan.spol,
+                              'aktivnost' : dan.aktivnost                             
+                              }
+        uporabnik.nov_dan(dan.teza,
+                          dan.visina,
+                          dan.starost,
+                          dan.spol,
+                          dan.aktivnost,
+                          datum
+                          )
+        seznam_uporabnikov.shrani(ime)
+        return bottle.redirect('/front-page/{}/{}'.format(ime, datum_str))
     else:
-        bottle.redirect('/vpis/')
+        return bottle.redirect('/front-page/{}/{}'.format(ime, datum_str))
+    
+    
+#######################################  DODAJ PAGE   #######################################
+
+@bottle.post('/fp-dodaj/<ime_uporabnika>/<datum>')
+def gumb_dodaj(ime_uporabnika, datum):
+    ime = bottle.request.get_cookie('ime', secret=SKRIVNOST)
+    return bottle.redirect('/front-page/{}/{}/dodaj'.format(ime_uporabnika, datum))
+
+@bottle.get('/front-page/<ime_uporabnika>/<datum>/dodaj')
+def dodaj(ime_uporabnika, datum):
+    return bottle.template('views/dodaj.tpl', ime_uporabnika=ime_uporabnika,
+                                              datum=datum)
+
+@bottle.post('/dodaj/<ime_uporabnika>/<datum>/')
+def dodaj(ime_uporabnika, datum):
+    ime = bottle.request.get_cookie('ime', secret=SKRIVNOST)
+    uporabnik = seznam_uporabnikov.uporabniki.get(ime_uporabnika)
+    seznam_dni = uporabnik.seznam_dni
+
+    hrana = bottle.request.forms.getunicode('hrana')
+    gram = float(bottle.request.forms.getunicode('gram'))
+
+    if hrana not in list(slovar_hrane.naredi_slovar_hrane().keys()):
+        return bottle.redirect('/front-page/{}/{}/ni'.format(ime_uporabnika, datum))
+    else:
+        datum_list = [int(x) for x in datum.split('-')]
+        dan_z_datumom(seznam_dni, datum_list).dodaj(hrana, gram)
+        seznam_uporabnikov.shrani(ime_uporabnika)
+        return bottle.redirect('/front-page/{}/{}'.format(ime_uporabnika, datum))
 
 
-@bottle.get('/front-page/<ime_uporabnika>/<id_dneva:int>')
-def front_page(ime_uporabnika, id_dneva):
-    return bottle.template('views/front-page.tpl', ime_uporabnika=ime_uporabnika, id_dneva=id_dneva)
+@bottle.get('/front-page/<ime_uporabnika>/<datum>/ni')
+def dodaj_ne_gre(ime_uporabnika, datum):
+    return bottle.template('views/dodaj_ne_gre.tpl', ime_uporabnika=ime_uporabnika,
+                                                     datum=datum)
 
+
+#######################################  DODAJ NA SEZNAM PAGE   #######################################
+
+@bottle.post('/<ime_uporabnika>/<datum>/odpri_na_seznam/')
+def gumb_dodaj_na_seznam(ime_uporabnika, datum):
+    return bottle.redirect('/{}/{}/dodaj_na_seznam'.format(ime_uporabnika, datum))
+
+@bottle.get('/<ime_uporabnika>/<datum>/dodaj_na_seznam')
+def dodaj_na_seznam(ime_uporabnika, datum):
+    slovar = slovar_hrane.naredi_slovar_hrane()
+    return bottle.template('views/dodaj_na_seznam.tpl', ime_uporabnika=ime_uporabnika,
+                                                        datum=datum,
+                                                        slovar=slovar)
+
+@bottle.post('/<ime_uporabnika>/<datum>/dodaj_na_seznam/')
+def dodaj_na_seznam(ime_uporabnika, datum):
+    hrana = bottle.request.forms.getunicode('hrana')
+    cal = bottle.request.forms.getunicode('cal')
+    oh = bottle.request.forms.getunicode('oh')
+    pro = bottle.request.forms.getunicode('pro')
+    mas = bottle.request.forms.getunicode('mas')
+
+    slovar_hrane.dodaj(hrana, cal, oh, pro, mas)
+    return bottle.redirect('/front-page/{}/{}'.format(ime_uporabnika, datum))
+
+
+#######################################  ODJAVA   #######################################
+
+@bottle.get('/odjava/')
+def sign_in():
+    return bottle.template('views/login.tpl')
+
+
+
+
+def naredi_seznam_dnevnih_osebnih_podatkov(datum):
+    ime = bottle.request.get_cookie('ime', secret=SKRIVNOST)
+    uporabnik = seznam_uporabnikov.uporabniki.get(ime)
+    dan = dan_z_datumom(uporabnik.seznam_dni, datum)
+    teza = dan.teza
+    visina = dan.visina
+    starost = dan.starost
+    spol = dan.spol
+    aktivnost = dan.aktivnost
+    return {'teza' : teza, 
+        'visina' : visina,
+        'starost' : starost,
+        'spol' : spol,
+        'aktivnost' : aktivnost}
+
+def dnevni_seznam_vrednosti(datum, ime):
+    uporabnik = seznam_uporabnikov.uporabniki[ime]
+    dan = dan_z_datumom(uporabnik.seznam_dni, datum)
+    return dan.seznam_vrednosti 
+
+def dnevni_slovar_hrane(datum, ime):
+    uporabnik = seznam_uporabnikov.uporabniki[ime]
+    dan = dan_z_datumom(uporabnik.seznam_dni, datum)
+    return dan.seznam_hrane
+
+def dan_z_datumom(seznam_dni, datum):
+    # seznam_dni = seznam_uporabnikov.uporabniki['Lucija'].seznam_dni
+    for sez in seznam_dni:
+        if sez.datum == datum:
+            return sez
 
 bottle.run(reloader=True, debug=True)
